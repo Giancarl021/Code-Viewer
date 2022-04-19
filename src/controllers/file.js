5af14346f827b0556a871c2638cf28e0b3d81003
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2022-04-19 11:15:59 +0000

    Fix encoded filenames

--- src/controllers/file.js
+++ src/controllers/file.js
@@ -2,7 +2,7 @@ const locate = require('@giancarl021/locate');
 
 module.exports = function (path) {
     return async (request, response) => {
-        const dest = locate(`${path}${request.path.replace(/^\/api\/file/, '')}`);
+        const dest = locate(`${path}${decodeURIComponent(request.path.replace(/^\/api\/file/, ''))}`);
 
         return response.sendFile(dest, {
             dotfiles: 'allow'
