86cfc49e01065dd5cd01e8f100b453f736fb3496
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2022-07-20 09:08:19 -0300

    Better auto-detect on files, new logo and wrapping

--- src/web/assets/js/main.js
+++ src/web/assets/js/main.js
@@ -5,21 +5,68 @@ async function main() {
         location.href = '/';
     }
 
+    const extension = f.split('.').pop();
+    const options = loadOptions();
+
     const response = await fetch(`/_/api/file${f}`);
 
     if (response.status !== 200) {
         location.href = '/';
     }
 
-    document.getElementById('go-back').onclick = () => history.go(-1);
-
+    
     const content = await response.text();
+    
+    const $data = document.getElementById('data');
+    const $goBack = document.getElementById('go-back');
+    const $toggleWrap = document.getElementById('toggle-wrap');
 
-    const data = document.getElementById('data');
+    $goBack.onclick = () => history.go(-1);
 
-    data.textContent = content;
+    updateWrap();
+    $toggleWrap.onclick = () => {
+        options.wrap = !options.wrap;
+        setOption('wrap', options.wrap);
+        updateWrap();
+    };
+
+    $data.textContent = content;
+
+    if (hljs.getLanguage(extension)) $data.classList.add(`language-${extension}`, extension);
 
     hljs.highlightAll();
+
+    function updateWrap() {
+        $data.classList[options.wrap ? 'add' : 'remove']('--wrap');
+        $toggleWrap.textContent = `${options.wrap ? 'Disable' : 'Enable'} wrapping`;
+    }
+}
+
+function loadOptions() {
+    const keys = [
+        {
+            name: 'wrap',
+            default: 'false',
+            parse: opt => opt === 'true'
+        }
+    ];
+
+    const options = {};
+
+    for (const item of keys)
+        options[item.name] = item.parse(valueOrDefault(localStorage.getItem(`opt:${item.name}`), item.default));
+
+    return options;
+}
+
+function setOption(key, value) {
+    localStorage.setItem(`opt:${key}`, value);
+}
+
+function valueOrDefault(value, defaultValue = null) {
+    return (value === null || value === undefined) ?
+        defaultValue :
+        value;
 }
 
 document.addEventListener('DOMContentLoaded', main);
